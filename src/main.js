81f1df8f75f062e0497f3bd324cb970bbcab1588
FrogIT <kimhg0149@gmail.com>
2019-05-08 19:19:04 +0900

    190508-01 업데이트
    Noto Sans 글꼴 적용, HTML 구조화, 애니메이션 효과 추가, 취소 버튼 추가, 디자인 대개편, 버튼 자동 숨김, textField 높이를 HTML rows 속성에서 CSS height 속성으로 변경, 새로운 메모 아이템은 이제 배열의 맨 앞에 추가됨(unshift), 수정 실수 방지 confirm, 취소 기능 추가, 배경 고정, 툴팁 텍스트 침범 방지, 외 자잘한 것들..

--- src/main.js
+++ src/main.js
@@ -1,12 +1,20 @@
 var memoApp = new Vue({
     el: 'main',
     data: {
-        titleField: '',
-        textField: '',
         items: [{id:0, text:'TIP - 상단의 입력 상자를 이용해 메모를 생성하세요.\n메모 위에 마우스를 올려 수정 또는 삭제할 수 있습니다.', time:new Date(2019, 0, 1, 0, 0, 0)}],
         nextItemNum: 1,
-        rows: 2,
-        edit: ''
+        edit: '',
+        titleField: '',
+        textField: '',
+        btnArea: '생성',
+        style: {
+            btnArea: {
+                display: 'none'
+            },
+            textField: {
+                height: '3em'
+            }
+        }
     },
     methods: {
         addItem: function(){
@@ -19,7 +27,7 @@ var memoApp = new Vue({
                 }
                 this.edit = ''
             } else { //NEW MODE
-                this.items.push({
+                this.items.unshift({
                     id: this.nextItemNum++,
                     title: this.titleField,
                     text: this.textField,
@@ -30,6 +38,9 @@ var memoApp = new Vue({
             this.textField = ''
         },
         editItem: function(item) {
+            if(this.textField) {
+                if(!confirm('입력 필드에 입력중인 내용이 있습니다. 입력중인 내용을 지우고 선택한 메모를 수정하시겠습니까?')) return
+            }
             this.titleField = item.title
             this.textField = item.text
             this.edit = item
@@ -38,6 +49,11 @@ var memoApp = new Vue({
 //            if(confirm(`해당 메모를 삭제하시겠습니까?\n메모 ID: ${item.id}`)) {
                 this.items.splice(this.items.indexOf(item), 1)
 //            }
+        },
+        cancelItem: function() {
+            this.titleField = ''
+            this.textField = ''
+            this.edit = ''
         },
         getTimesince: function(date) {
             var seconds = Math.floor((new Date() - date) / 1000);
@@ -70,16 +86,37 @@ var memoApp = new Vue({
     watch: {
         textField: function() {
             if (this.textField == '') {
-                this.rows = 2
+                this.style.textField.height = '3em'
+                this.style.btnArea.display = 'none'
             }
             else {
-                this.rows = 8
+                this.style.textField.height = '12em'
+                this.style.btnArea.display = 'inline-block'
+            }
+        },
+        edit: function() {
+            if(this.edit) {
+                this.btnArea = '수정'
+            } else {
+                this.btnArea = '생성'
             }
         }
     },
     components: {
         memoItem: {
-            template: `<div><span style="display:block;font-size:1.5em;">{{title}}</span><pre>{{text}}</pre><br>{{timesince}}<div class="tooltiptext"><b>메모 ID</b> {{id}}<br><b>수정됨</b> {{timefull}}<br><span class="link" @click="$emit('edit')">수정</span> <span class="link" @click="$emit('remove')">삭제</span></div></div>`,
+            template: `
+                <div>
+                    <span style="display:block;font-size:1.5em;">{{title}}</span>
+                    <pre>{{text}}</pre><br>
+                    {{timesince}}
+                    <div class="tooltiptext">
+                        <b>메모 ID</b> {{id}}<br>
+                        <b>수정됨</b> {{timefull}}<br>
+                        <span class="link" @click="$emit('edit')">수정</span>
+                        <span class="link" @click="$emit('remove')">삭제</span>
+                    </div>
+                </div>
+            `,
             props: ['id', 'title', 'text', 'timesince', 'timefull']
         }
     }
